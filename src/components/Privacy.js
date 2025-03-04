4c3354ed9ef70f0a1635a90ce2fc9f69af9b6ffc
Jason Kim <jason.kim7288@gmail.com>
2025-03-04 20:32:50 +1000

    update privacy

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -15,16 +15,28 @@ const Privacy = () => {
   return (
     <div style={privacyStyles}>
       <h3>{t('Privacy Policy')}</h3>
-      <p>{t('Privacy Policy main description')}</p>
-      <h5>{t('Analytics')}</h5>
-      <p>{t('Analytics description')}</p>
-      <h5>{t('Crash reports')}</h5>
-      <p>{t('Crash reports description')}</p>
-      {/* <h5>{t('Advertising')}</h5>
-      <p>{t('Advertising description')}</p> */}
-      <h5>{t('Changes to this policy')}</h5>
-      <p>{t('Changes to this policy description')}</p>
-      <p className="mt-5">{t('Last modified with the date')}</p>
+      <p>{t('Privacy Policy 1')}</p>
+      <p>{t('Privacy Policy 2')}</p>
+      <h5>{t('Header 1')}</h5>
+      <p>{t('Details 1-1')}</p>
+      <h5>{t('Header 2')}</h5>
+      <p>{t('Details 2-1')}</p>
+      <ul>
+        <li>{t('Details 2-2')}</li>
+        <li>{t('Details 2-3')}</li>
+        <li>{t('Details 2-4')}</li>
+      </ul>
+      <p>{t('Details 2-5')}</p>
+      <h5>{t('Header 3')}</h5>
+      <p>{t('Details 3-1')}</p>
+      <ul>
+        <li>{t('Details 3-2')}</li>
+        <li>{t('Details 3-3')}</li>
+        <li>{t('Details 3-4')}</li>
+      </ul>
+      <h5>{t('Header 4')}</h5>
+      <p>{t('Details 4-1')}</p>
+      <p>{t('Details 4-2')}</p>
     </div>
   );
 };
